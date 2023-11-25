d30c36204b68f3bcd4a18df3ba90bc7c11657ae6
Achraf <achrafbensebaa23@gmail.com>
2023-11-25 12:50:12 +0100

    add_arc + fix gmap

--- src/tools.ml
+++ src/tools.ml
@@ -2,7 +2,12 @@ open Graph
 
 let clone_nodes gr= n_fold gr new_node empty_graph
 let gmap gr f = 
-  e_fold gr (fun g arc ->  (new_arc g {src=arc.src; tgt=arc.tgt; lbl=f arc.lbl})) empty_graph
+  e_fold gr (fun g arc ->  (new_arc g {src=arc.src; tgt=arc.tgt; lbl=f arc.lbl})) (clone_nodes gr)
 
-let add_arc _a _b _c _d = assert false
+let add_arc graph id1 id2 n = 
+  let arc =find_arc graph id1 id2 in
+  match arc with
+  | None -> new_arc graph {src=id1; tgt=id2; lbl=n}
+  | Some arc -> new_arc graph {src=arc.src; tgt=arc.tgt; lbl=arc.lbl+n}
+
 
