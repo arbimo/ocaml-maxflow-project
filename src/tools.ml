5e92d25ce1cdec46ef80cd69569b2b71ec628dec
AnaGrskovic <ana.grskovic56@gmail.com>
2023-11-27 12:09:28 +0100

    Work on flow graph methods

--- src/tools.ml
+++ src/tools.ml
@@ -1,6 +1,7 @@
 open Graph
 
 let clone_nodes gr = n_fold gr new_node empty_graph
+
 let gmap gr f = 
   e_fold gr (fun g arc ->  (new_arc g {src=arc.src; tgt=arc.tgt; lbl=f arc.lbl})) (clone_nodes gr)
 
@@ -10,4 +11,19 @@ let add_arc graph id1 id2 n =
   | None -> new_arc graph {src=id1; tgt=id2; lbl=n}
   | Some arc -> new_arc graph {src=arc.src; tgt=arc.tgt; lbl=arc.lbl+n}
 
-let create_flow_graph gr = e_fold gr (fun g arc ->  (new_arc g {src=arc.src; tgt=arc.tgt; lbl=0})) (clone_nodes gr)
+let create_flow_graph graph = gmap graph (fun _ -> 0)
+
+let update_arc_flow graph src_id dest_id flow = 
+  match (find_arc graph src_id dest_id) with
+  | Some arc -> arc.lbl <- arc.lbl + flow
+  | None -> ()
+
+(*let update_flow_graph graph node_ids new_flow = 
+  let mylength_acu_better l =
+    let rec loop acu l = match l with
+      | Empty -> acu
+      | Cell(_, tail) -> loop (acu + 1) tail
+    in
+    loop 0 l ;;*)
+
+(*let find_max_possible_flow graph node_ids = e_fold graph (fun acu arc -> if List.mem arc.src myList) 0*)
--- src/tools.mli
+++ src/tools.mli
@@ -1,7 +1,16 @@
 open Graph
 
 val clone_nodes: 'a graph -> 'b graph
+
 val gmap: 'a graph -> ('a -> 'b) -> 'b graph
+
 val add_arc: int graph -> id -> id -> int -> int graph
 
+(* try not to lose genericity *)
 val create_flow_graph: 'a graph -> int graph
+
+val update_arc_flow: int graph -> int -> int -> int -> int graph
+
+(*val update_flow_graph: int graph -> int list -> int -> int graph*)
+
+(*val find_max_possible_flow: int graph -> int list -> int*)
