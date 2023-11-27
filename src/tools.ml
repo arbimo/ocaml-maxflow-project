16094291a07506ec13164d6193e1ada82ef1cacb
AnaGrskovic <ana.grskovic56@gmail.com>
2023-11-27 11:14:01 +0100

    Add method for creating a flow graph

--- src/tools.ml
+++ src/tools.ml
@@ -1,13 +1,13 @@
 open Graph
 
-let clone_nodes gr= n_fold gr new_node empty_graph
+let clone_nodes gr = n_fold gr new_node empty_graph
 let gmap gr f = 
   e_fold gr (fun g arc ->  (new_arc g {src=arc.src; tgt=arc.tgt; lbl=f arc.lbl})) (clone_nodes gr)
 
 let add_arc graph id1 id2 n = 
-  let arc =find_arc graph id1 id2 in
+  let arc = find_arc graph id1 id2 in
   match arc with
   | None -> new_arc graph {src=id1; tgt=id2; lbl=n}
   | Some arc -> new_arc graph {src=arc.src; tgt=arc.tgt; lbl=arc.lbl+n}
 
-
+let create_flow_graph gr = e_fold gr (fun g arc ->  (new_arc g {src=arc.src; tgt=arc.tgt; lbl=0})) (clone_nodes gr)
--- src/tools.mli
+++ src/tools.mli
@@ -3,3 +3,5 @@ open Graph
 val clone_nodes: 'a graph -> 'b graph
 val gmap: 'a graph -> ('a -> 'b) -> 'b graph
 val add_arc: int graph -> id -> id -> int -> int graph
+
+val create_flow_graph: 'a graph -> int graph
